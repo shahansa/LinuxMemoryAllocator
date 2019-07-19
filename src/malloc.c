786784f0eb6a42aa93b2959f0d03cfcf6a977073
shahanshasalim <shehan492@outlook.com>
2019-07-19 17:05:40 +0100

    added basic sbrk call for malloc

--- src/malloc.c
+++ src/malloc.c
@@ -9,12 +9,61 @@
 //todo : move all define and typedef to common.h
 
 #define FREE_BLOCK -1
-#define ALLOC_BLOCK 0
+#define ALLOCATED_BLOCK 0
 
 #include "../include/common.h"
+#include <assert.h>
+
+/*
++----------------+-------------------------------------++
+|free?  |pointer |                                     |
++----------------+                                     |
+| Block Meta data|  Actual memory block                |
+|                |  (Actual memory requested by the    |
+|(This is extra  |  program)                           |
+|space used by   |                                     |
+|every block)    |                                     |
+|                |                                     |
+|                |                                     |
++----------------+-----------+-------------------------+
+ Total size = Requested Size + Block meta data size
+*/
 
 typedef struct BlockMetaData{
 	size_t block_size;
 	struct BlockMetaData *next;
 	int status;
+}BlockMeta;
+
+#define BLOCK_META_SIZE sizeof(BlockMeta)
+
+void *head = NULL;
+
+BlockMeta requestToOperatingSystem(size_t size)
+{
+	/*refer to man page for sbrk */
+	size_t requestSize = size + BLOCK_META_SIZE;
+	presentBreak = sbrk(0);
+	allocatedBlock = sbrk(requestSize);
+	if((void*)-1 == allocatedBlock)
+		return NULL;
+	presentBreak -> block_size = requestSize;
+	presentBreak -> status = ALLOCATED_BLOCK;
+	presentBreak -> next = NULL;
+	return(presentBreak);
 }
+
+void* malloc(size_t size)
+{
+	assert(size > 0);
+	if(NULL == head){ /* this is the first request since program start*/
+		head = requestToOperatingSystem(size);
+		return(head);
+	}
+	else{
+		//allocate and do other stuff tomorrow 
+	}
+}
+
+
+	
